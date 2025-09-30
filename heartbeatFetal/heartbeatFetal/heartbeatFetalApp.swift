b94a22b8c6a990b3b5a53ce165ee8eef245abb57
destucr <destucr@gmail.com>
2025-09-30 13:25:30 +0700

    init commit

---
+++ heartbeatFetal/heartbeatFetal/heartbeatFetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartbeatFetalApp.swift
+//  heartbeatFetal
+//
+//  Created by Destu Cikal Ramdani on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct heartbeatFetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
